718edd2b5b4ed809c6d102f83af959153adefd3b
Joseba Merino <yosiunico@gmail.com>
2020-02-13 16:33:39 +0100

    Creado navbar con cosas recicladas

--- src/app/componentes/navbar/navbar.component.ts
+++ src/app/componentes/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+
+  isNavbarCollapsed: boolean;
+  numero: number;
+  rutas: Array<any>;
+
+  isLogged: boolean;
+
+  constructor(public usuarioService: UsuarioService, private router: Router) {
+    console.trace('NavbarComponent constructor');
+    this.rutas = RUTAS;
+    this.isNavbarCollapsed = true;
+    this.numero = 0;
+
+    this.isLogged = false;
+  }// constructor
 
   ngOnInit() {
+    //this.isLogged = this.usuarioService.estaLogeado();
+
+    // Cada vez que cambia la URL actualiza la session (solucion no muy buena)
+    this.router.events.subscribe((val) => {
+      if (val instanceof NavigationEnd) {
+        this.isLogged = this.usuarioService.estaLogeado();
+      }
+
+    });
   }
 
+  alternarOculto() {
+    if(this.isNavbarCollapsed) {
+      this.isNavbarCollapsed = false;
+      this.empezarMostrar();
+    } else {
+      this.empezarOcultar();
+    }
+  }
+
+  empezarMostrar(){
+    if(this.numero < this.rutas.length - 1 ){
+      this.numero ++;
+      setTimeout(() => { this.empezarMostrar() }, TIME_SHOW);
+    } else {
+      this.numero++;
+    }
+  }
+
+  empezarOcultar(){
+    if(this.numero > 1){
+      this.numero --;
+      setTimeout(() => { this.empezarOcultar() }, TIME_SHOW);
+    } else {
+      this.numero--;
+      this.isNavbarCollapsed = true;
+    }
+  }
+
+  salir() {
+    console.trace('NavbarComponent click boton cerrar sesion');
+
+    const mensaje = '¿Estas seguro de que quieres salir?';
+    if(confirm(mensaje)){
+      this.usuarioService.cerrarSesion(99);
+      this.router.navigate(['login']);
+    }
+  }
 }
