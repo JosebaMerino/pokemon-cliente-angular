4fa3da2196fc0269e700fdae404693d5dd800fa5
Joseba <joseba.merino.pina@gmail.com>
2020-02-17 10:37:15 +0100

    Creado parcialmente pantalla inicial del backoffice

--- src/app/paginas/backoffice/backoffice.component.ts
+++ src/app/paginas/backoffice/backoffice.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { Pokemon } from 'src/app/model/pokemon';
+import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-backoffice',
   templateUrl: './backoffice.component.html',
   styleUrls: ['./backoffice.component.scss']
 })
+
 export class BackofficeComponent implements OnInit {
 
-  constructor() { }
+  alerta: Alerta;
+  pokemons: Array<Pokemon>;
+  pokemonSelecionado: Pokemon;
+
+  constructor(private pokemonService: PokemonService) {
+    this.alerta = new Alerta();
+    this.pokemons = new Array<Pokemon>();
+    this.pokemonSelecionado = undefined;
+  }// constructor
 
   ngOnInit() {
+    this.pokemonService.getPokemons().subscribe(
+      (pokemons) => {
+        this.pokemons = pokemons;
+      }
+    );
+  }// ngOnInit
+
+  selecionarPokemon(pokemon: Pokemon) {
+    console.trace('Pokemon selecconado : %o', pokemon);
+    this.pokemonSelecionado = pokemon;
   }
 
-}
+
+
+}// BackofficeComponent
+
+
+class Alerta {
+  mostrandose: boolean;
+  mensaje: string;
+
+  constructor() {
+    this.mostrandose = false;
+    this.mensaje = '';
+  }
+}// Alerta
