315832b96933757d0b4ab9437c52a7f2ee8b4b51
Joseba <joseba.merino.pina@gmail.com>
2020-02-20 10:46:04 +0100

    Preparado formulario de habilidades

--- src/app/paginas/crear-pokemon/crear-pokemon.component.ts
+++ src/app/paginas/crear-pokemon/crear-pokemon.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, Validators, FormArray, AbstractControl } from '@angular/forms';
 import { Pokemon } from 'src/app/model/pokemon';
 import { PokemonService } from 'src/app/services/pokemon.service';
+import { CheckItem } from 'src/app/model/checkItem';
 
 @Component({
   selector: 'app-crear-pokemon',
@@ -12,25 +13,72 @@ export class CrearPokemonComponent implements OnInit {
 
   formulario: FormGroup;
 
+  options: Array<CheckItem>;
+  habilidadesForm: FormArray;
+
   constructor(private builder: FormBuilder, private pokemonService : PokemonService) {
+    this.crearFormulario();
+
+    this.options = new Array<CheckItem>();
+    this.options.push(new CheckItem('Impasible', 1));
+    this.options.push(new CheckItem('Rayos', 2));
+    this.options.push(new CheckItem('Oloroso', 3));
+  }
+
+  crearFormulario() {
     this.formulario = this.builder.group({
-      nombre: ['']
+      id: new FormControl(0),
+      nombre: new FormControl('',
+                              Validators.compose(
+                                  [
+                                    Validators.required,
+                                    Validators.minLength(2),
+                                    Validators.maxLength(50)
+                                  ])
+                              ),
+      habilidades:  this.builder.array([ ], // Creamos array sin habilidades
+                                    Validators.compose(
+                                    [
+                                      Validators.required,
+                                      Validators.minLength(1)
+                                    ])
+                                )
     });
+    this.habilidadesForm = this.formulario.get('habilidades') as FormArray;
   }
 
   ngOnInit() {
   }
 
-  enviar(values : any) {
-    console.debug(values.nombre);
-    const pokemon = new Pokemon();
+  enviar(value: any) {
 
-    pokemon.nombre = values.nombre;
-    this.pokemonService.postPokemon(pokemon).subscribe(
-      (dato)=> {
-        console.debug(dato);
-      }
-    );
+    console.debug('Submitado!');
+    const pokemon = new Pokemon();
+    pokemon.id = value.id;
+    pokemon.nombre = value.nombre;
   }
 
-}
+  private crearFormGroupHabilidad(nombre: string, id: number): FormGroup {
+    return this.builder.group({
+      id: new FormControl(id),
+      nombre: new FormControl(nombre)
+    });
+  } // crearFormGroupHabilidad
+
+  checkCambiado(option: CheckItem) {
+    console.debug('Se ha seleccionado : %o', option);
+
+    if (option.checked) {
+      // Se crea una nueva habilidad
+      const habilidad = this.crearFormGroupHabilidad(option.name, option.value);
+      this.habilidadesForm.push(habilidad);
+    } else {
+      // Se borra la habilidad de la lista
+      this.habilidadesForm.removeAt(this.habilidadesForm.value.findIndex(habilidad => habilidad.id === option.value));
+    }
+  } // checkCambiado(option)
+
+
+  // BY MIKEL BUA SAPI --> Cargar habilidades
+
+} // Crear pokemon
