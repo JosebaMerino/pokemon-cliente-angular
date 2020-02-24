259b3440c746ad192fda375ba6777e477522b6c5
Joseba <joseba.merino.pina@gmail.com>
2020-02-24 12:21:31 +0100

    Implementado login usando servicio REST

--- src/app/paginas/login/login.component.ts
+++ src/app/paginas/login/login.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UsuarioService } from 'src/app/services/usuario.service';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Usuario } from 'src/app/model/usuario';
+
+const KEY_ISLOGGED = 'isLogged';
+
 
 @Component({
   selector: 'app-login',
@@ -12,11 +16,14 @@ export class LoginComponent implements OnInit {
 
   formulario: FormGroup;
 
+  private storage: Storage;
+
+
   constructor(private router: Router,
               private usuarioService: UsuarioService,
               private builder: FormBuilder
               ) {
-
+                this.storage = window.sessionStorage;
   } // constructor
 
   ngOnInit() {
@@ -29,9 +36,19 @@ export class LoginComponent implements OnInit {
   enviar( values) {
 
 
-    this.usuarioService.login(values.username, values.password);
-
-    this.router.navigate(['/backoffice']);
+    this.usuarioService.login(values.username, values.password).subscribe(
+      () => {
+          const usuarioBuscar = new Usuario();
+          usuarioBuscar.nombre = values.username;
+          usuarioBuscar.password = values.password;
+          usuarioBuscar.id = 99;
+          this.storage.setItem(KEY_ISLOGGED, JSON.stringify(usuarioBuscar)); // marcar que esta logeado
+          this.router.navigate(['/backoffice']);
+      },
+      (error) => {
+        console.trace('No se ha podido logear');
+      }
+    );
   }// login
 
 }
